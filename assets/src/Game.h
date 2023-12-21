cb27e4e7eb658ef3752c1727098985eb095dfe70
Emmett-Lim <emmettklim@csu.fullerton.edu>
2023-12-21 01:11:02 -0800

    Added new file(s) named Vector2.

--- assets/src/Game.h
+++ assets/src/Game.h
@@ -3,30 +3,31 @@
 #include "utilities.h"
 #include "Plane.h"
 #include "Mouse.h"
+#include "Vector2.h"
 
 class Game {
 
-private:
-
-	void HandleEvents();
-	void Update();
-	void Draw();
-
-	bool is_running_{ true };
-
-	SDL_Window* window_{ nullptr };
-	SDL_Renderer* renderer_{ nullptr };
-	SDL_Texture* texture_{ nullptr };
-
-	Mouse mouse_;
-
-	Plane plane_a{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 57.0, 3, false };			// Test
-	Plane plane_b{ SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 50.0, 5, false };			// Test
-
-public:
-
-	bool Init();
-	void GameLoop();
-	void Close();
+	private:
+	
+		void HandleEvents();
+		void Update();
+		void Draw();
+	
+		bool is_running_{ true };
+	
+		SDL_Window* window_{ nullptr };
+		SDL_Renderer* renderer_{ nullptr };
+		SDL_Texture* texture_{ nullptr };
+	
+		Mouse mouse_;
+	
+		Plane plane_a{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 57.0, 3, false };			// Test
+		Plane plane_b{ SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 50.0, 5, false };			// Test
+	
+	public:
+	
+		bool Init();
+		void GameLoop();
+		void Close();
 
 };
---
+++ assets/src/Vector2.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "utilities.h"
+
+class Vector2 {
+
+	private:
+	
+	
+	public:
+
+};
